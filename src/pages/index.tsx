81cf87caa288d4a9c366e0dbc17e05a5818a1924
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-07-29 15:41:31 +0200

    Added more to the project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Hello World !</h1>
+    </div>
+  );
+};
+
+export default Home;
