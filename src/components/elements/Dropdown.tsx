89094ab89c9f821295a382d36ff55f745bc46527
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-05 20:14:46 +0200

    Added a readonly property on the dropdown

--- src/components/elements/Dropdown.tsx
+++ src/components/elements/Dropdown.tsx
@@ -4,10 +4,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 
 import useClickOutside from 'hooks/useClickOutside';
+import clsx from 'clsx';
 
 interface IProps {
   placeholder: string;
   label: string;
+
+  readonly?: boolean;
 
   className?: string;
 
@@ -16,10 +19,12 @@ interface IProps {
   setValue: React.Dispatch<React.SetStateAction<any>>;
 }
 
-const Dropdown: FunctionComponent<IProps> = ({ className, label, placeholder = 'Dropdown', values, value, setValue }: IProps) => {
+const Dropdown: FunctionComponent<IProps> = ({ className, label, placeholder = 'Dropdown', values, value, setValue, readonly }: IProps) => {
   const [isHidden, setHidden] = useState(true);
 
   const onChange = (slug: string): void => {
+    if (readonly) return;
+
     setValue(slug);
     closeDropdown();
   };
@@ -29,7 +34,7 @@ const Dropdown: FunctionComponent<IProps> = ({ className, label, placeholder = '
   };
 
   const switchState = (): void => {
-    setHidden((prev) => !prev);
+    !readonly && setHidden((prev) => !prev);
   };
 
   const handleClickOutside = (): void => {
@@ -45,8 +50,8 @@ const Dropdown: FunctionComponent<IProps> = ({ className, label, placeholder = '
           <p>{label}</p>
         </div>
 
-        <div className="relative" ref={container}>
-          <div className="z-20 relative border border-gray-500 rounded-sm w-full py-2 px-3 mt-2 cursor-pointer" onClick={switchState}>
+        <div className={clsx(['relative', readonly ? 'cursor-not-allowed' : 'cursor-pointer'])} ref={container}>
+          <div className="z-20 relative border border-gray-500 rounded-sm w-full py-2 px-3 mt-2" onClick={switchState}>
             <p className={`${value === '' || value === null ? 'text-gray-600' : 'text-black'} font-normal`}>
               {value ? values.find(({ slug }) => slug === value)?.name : placeholder}
             </p>
@@ -59,7 +64,7 @@ const Dropdown: FunctionComponent<IProps> = ({ className, label, placeholder = '
           {isHidden ? null : (
             <div className="z-30 absolute top-full left-0 bg-white border border-t-0 border-gray-500 rounded-sm rounded-t-none w-full pt-2">
               {values.map(({ name, slug }) => (
-                <div className="group rounded-sm w-full py-1 px-1 cursor-pointer" onClick={() => onChange(slug)} key={uuidv4()}>
+                <div className="group rounded-sm w-full py-1 px-1" onClick={() => onChange(slug)} key={uuidv4()}>
                   <p className="text-gray-600 font-normal text-sm py-1 px-2 rounded-sm group-hover:bg-gray-200 transition-all duration-300">{name}</p>
                 </div>
               ))}
