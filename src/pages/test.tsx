ad0cdcff0a0c222276d2baa71a11bab838463c07
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-08 11:47:25 +0200

    Added a test page for development

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const Test = (): ReactElement => {
+  return (
+    <div className="flex w-full h-full">
+      <div className="flex flex-row m-auto gap-7"></div>
+    </div>
+  );
+};
+
+export default Test;
