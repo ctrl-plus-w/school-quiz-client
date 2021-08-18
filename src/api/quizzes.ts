cba094430613ddf50246ebb66748de582dedab6a
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-18 14:29:13 +0200

    Added get query on the api files

--- src/api/quizzes.ts
+++ src/api/quizzes.ts
@@ -6,6 +6,39 @@ import database from '@database/database';
 
 const DEFAULT_API_ERROR_RESPONSE: APIResponse<null> = [null, { status: 400, message: 'Une erreur est survenue ' }];
 
+export const getQuizzes = async (token: string): Promise<APIResponse<Array<IQuiz>>> => {
+  try {
+    const { data: quizzes } = await database.get('/api/quizzes', getHeaders(token));
+
+    return [quizzes, undefined];
+  } catch (_err) {
+    const err = _err as AxiosError;
+
+    if (!err.response) return DEFAULT_API_ERROR_RESPONSE;
+
+    if (err.response.status === 403) return [null, { status: 403, message: '' }];
+
+    return DEFAULT_API_ERROR_RESPONSE;
+  }
+};
+
+export const getQuiz = async (quizId: number, token: string, userId?: number): Promise<APIResponse<IQuiz>> => {
+  try {
+    const { data: quiz } = await database.get(userId ? `/api/users/${userId}/quizzes/${quizId}` : `/api/quizzes/${quizId}`, getHeaders(token));
+
+    return [quiz, undefined];
+  } catch (_err) {
+    const err = _err as AxiosError;
+
+    if (!err.response) return DEFAULT_API_ERROR_RESPONSE;
+
+    if (err.response.status === 403) return [null, { status: 403, message: '' }];
+    if (err.response.status === 404) return [null, { status: 404, message: "Le quiz n'a pas été trouvé !" }];
+
+    return DEFAULT_API_ERROR_RESPONSE;
+  }
+};
+
 export const createQuiz = async (creationAttributes: QuizCreationAttributes, token: string): Promise<APIResponse<IQuiz>> => {
   try {
     const { data: quiz } = await database.post('/api/quizzes', creationAttributes, getHeaders(token));
