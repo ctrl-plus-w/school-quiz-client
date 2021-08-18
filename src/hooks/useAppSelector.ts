0d95a43c13954301c95d1af1a0097ece5d8fbcce
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-18 14:28:57 +0200

    Fixed the naming on the useAppSelector hook

--- src/hooks/useAppSelector.ts
+++ src/hooks/useAppSelector.ts
@@ -2,6 +2,6 @@ import { useSelector, TypedUseSelectorHook } from 'react-redux';
 
 import { RootState } from '@redux/store';
 
-const selector: TypedUseSelectorHook<RootState> = useSelector;
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export default selector;
+export default useAppSelector;
