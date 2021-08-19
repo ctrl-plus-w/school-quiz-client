cf9c480bbcd6399be01d454fc5f2d4275fa15186
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-19 16:48:16 +0200

    Added a small property on the input skeleton

--- src/components/skeletons/InputSkeleton.tsx
+++ src/components/skeletons/InputSkeleton.tsx
@@ -10,16 +10,18 @@ interface IProps {
   textArea?: boolean;
 
   className?: string;
+
+  small?: boolean;
 }
 
 // ! Using 'box-content my-px' to simulate the space created on the real real input w/ the border.
 
-const InputSkeleton = ({ maxLength, textArea }: IProps): ReactElement => {
+const InputSkeleton = ({ maxLength, small, textArea }: IProps): ReactElement => {
   return (
     <div className={clsx(['form-control flex flex-col w-80'])}>
       <div className="text-sm font-semibold text-gray-900">
         <div>
-          <TextSkeleton width={32} height={5} />
+          <TextSkeleton width={small ? 12 : 32} height={5} />
         </div>
 
         <div className={clsx(['w-full box-content py-px mt-2 bg-gradient-to-br from-gray-300 to-gray-100 rounded', textArea ? 'h-17' : 'h-9'])}></div>
