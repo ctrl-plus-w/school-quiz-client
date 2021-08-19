068b520ad83f4f14047d6965fedc4fa76301ad8b
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-19 19:30:22 +0200

    Removed the contextes

--- src/context/AuthContext/AuthContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export const contextDefaultValues: AuthContextState = {
-  token: '',
-  setToken: (token: string) => null,
-};
-
-export const AuthContext = createContext<AuthContextState>(contextDefaultValues);
--- src/context/NotificationContext/NotificationContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-export const contextDefaultValues: NotificationContextState = {
-  notifications: [],
-  addNotification: (notifications: UINotification) => null,
-  removeNotification: (notifications: UINotification) => null,
-};
-
-export const NotificationContext = createContext<NotificationContextState>(contextDefaultValues);
