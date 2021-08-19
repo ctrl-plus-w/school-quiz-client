257c24b24cbd02edc43a5ec4a68b8e4befa78b67
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-19 20:38:49 +0200

    Added a tempEvent on the event slice

--- src/redux/eventSlice.ts
+++ src/redux/eventSlice.ts
@@ -6,10 +6,14 @@ import { RootState } from '@redux/store';
 
 interface IEventState {
   events: Array<IEvent>;
+
+  tempEvent: IEvent | null;
 }
 
 const initialState: IEventState = {
   events: [],
+
+  tempEvent: null,
 };
 
 const addEventsReducer = (state: IEventState, action: PayloadAction<Array<IEvent>>) => {
@@ -24,6 +28,14 @@ const clearEventsReducer = (state: IEventState) => {
   state.events = [];
 };
 
+const setTempEventReducer = (state: IEventState, action: PayloadAction<IEvent>) => {
+  state.tempEvent = action.payload;
+};
+
+const clearTempEventReducer = (state: IEventState) => {
+  state.tempEvent = null;
+};
+
 const eventSlice = createSlice({
   name: 'event',
 
@@ -33,11 +45,14 @@ const eventSlice = createSlice({
     addEvents: addEventsReducer,
     removeEvent: removeEventReducer,
     clearEvents: clearEventsReducer,
+    setTempEvent: setTempEventReducer,
+    clearTempEvent: clearTempEventReducer,
   },
 });
 
-export const { addEvents, removeEvent, clearEvents } = eventSlice.actions;
+export const { addEvents, removeEvent, clearEvents, clearTempEvent, setTempEvent } = eventSlice.actions;
 
-export const selectEvents = (state: RootState): Array<IEvent> | null => state.event.events;
+export const selectEvents = (state: RootState): Array<IEvent> => state.event.events;
+export const selectTempEvent = (state: RootState): IEvent | null => state.event.tempEvent;
 
 export default eventSlice.reducer;
