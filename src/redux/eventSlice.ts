a962c87e7acc4933d314409a43541fe4581c7154
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-19 17:53:49 +0200

    Adde a redux remove event reducer

--- src/redux/eventSlice.ts
+++ src/redux/eventSlice.ts
@@ -16,6 +16,10 @@ const addEventsReducer = (state: IEventState, action: PayloadAction<Array<IEvent
   state.events = state.events.concat(action.payload);
 };
 
+const removeEventReducer = (state: IEventState, action: PayloadAction<number>) => {
+  state.events = state.events.filter((event) => event.id !== action.payload);
+};
+
 const clearEventsReducer = (state: IEventState) => {
   state.events = [];
 };
@@ -27,11 +31,12 @@ const eventSlice = createSlice({
 
   reducers: {
     addEvents: addEventsReducer,
+    removeEvent: removeEventReducer,
     clearEvents: clearEventsReducer,
   },
 });
 
-export const { addEvents, clearEvents } = eventSlice.actions;
+export const { addEvents, removeEvent, clearEvents } = eventSlice.actions;
 
 export const selectEvents = (state: RootState): Array<IEvent> | null => state.event.events;
 
