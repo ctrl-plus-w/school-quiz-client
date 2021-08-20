deb458880428c806f4b127eebb7982a996ede536
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-20 15:52:56 +0200

    Fixed a condition utils function

--- src/utils/condition.utils.ts
+++ src/utils/condition.utils.ts
@@ -1,5 +1,5 @@
 export const areArraysEquals = (arr: Array<any>, arr1: Array<any>, sortFunction?: (a: any, b: any) => number): boolean => {
-  return JSON.stringify(arr.sort(sortFunction)) === JSON.stringify(arr1.sort(sortFunction));
+  return JSON.stringify([...arr].sort(sortFunction)) === JSON.stringify([...arr1].sort(sortFunction));
 };
 
 export const areDatesEquals = (date: Date, date1: Date): boolean => {
