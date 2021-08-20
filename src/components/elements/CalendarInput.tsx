c3c8cc3e2ae9e9e469ade355f7c4b9d4ec494f1a
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-20 15:52:32 +0200

    Factorized, fixed and added a feature on the calendar input

--- src/components/elements/CalendarInput.tsx
+++ src/components/elements/CalendarInput.tsx
@@ -10,14 +10,64 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
 
 import useClickOutside from '@hooks/useClickOutside';
 
-import { formatDate, getCalendarDates, incrementMonth, isDateInMonth, isSameDate, monthToString, setDate } from '@util/date.utils';
+import { formatDate, getCalendarDates, incrementMonth, isDateInMonth, isSameDate, monthToString, setDate, setTime } from '@util/date.utils';
 import { areDatesEquals } from '@util/condition.utils';
 import { sliceArray } from '@util/array.utils';
 
 import { DAYS } from '@constant/date';
 
+interface ICellProps {
+  currentDate: Date;
+  cellDate: Date;
+
+  onlyFuture: boolean;
+
+  handleClick: (date: Date) => void;
+}
+
+const Cell = ({ currentDate, cellDate, onlyFuture, handleClick }: ICellProps): ReactElement => {
+  const defaultClasses = useState('flex justify-center items-center border border-transparent rounded-sm flex-grow w-10 py-1 ');
+
+  if (areDatesEquals(currentDate, cellDate)) {
+    return (
+      <div className={clsx([defaultClasses, 'bg-blue-600 text-white'])}>
+        <p>{cellDate.getDate()}</p>
+      </div>
+    );
+  }
+
+  if (onlyFuture && cellDate.valueOf() <= setTime(new Date(), 24, 0, 0, 0).valueOf()) {
+    return (
+      <div
+        className={clsx([
+          defaultClasses,
+          'cursor-not-allowed',
+          isSameDate(new Date(), cellDate) ? 'bg-gray-400 text-gray-700' : 'bg-gray-100 text-gray-400',
+        ])}
+      >
+        {cellDate.getDate()}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className={clsx([
+        defaultClasses,
+        'cursor-pointer',
+        isDateInMonth(currentDate, cellDate) ? 'bg-gray-200 text-gray-700 hover:border-gray-500' : 'bg-gray-100 text-gray-400 hover:border-gray-400',
+      ])}
+      onClick={handleClick.bind(this, cellDate)}
+    >
+      {cellDate.getDate()}
+    </div>
+  );
+};
+
 interface IProps {
   label: string;
+
+  onlyFuture?: boolean;
 
   className?: string;
 
@@ -25,7 +75,7 @@ interface IProps {
   setValue: Dispatch<SetStateAction<Date>>;
 }
 
-const CalendarInput = ({ label, className, value, setValue }: IProps): ReactElement => {
+const CalendarInput = ({ label, className, value, setValue, onlyFuture = false }: IProps): ReactElement => {
   const [hidden, setHidden] = useState(true);
 
   const { container } = useClickOutside<HTMLDivElement>(() => setHidden(true));
@@ -53,6 +103,7 @@ const CalendarInput = ({ label, className, value, setValue }: IProps): ReactElem
             <div className="flex justify-between items-center mb-3">
               <button
                 className="p-2 hover:text-blue-600 transform active:scale-75 transition-all duration-300"
+                type="button"
                 onClick={() => setValue((prev) => setDate(incrementMonth(prev, -1), 1))}
               >
                 <ChevronLeftIcon className="w-5 h-5" />
@@ -64,6 +115,7 @@ const CalendarInput = ({ label, className, value, setValue }: IProps): ReactElem
 
               <button
                 className="p-2 hover:text-blue-600 transform active:scale-75 transition-all duration-300"
+                type="button"
                 onClick={() => setValue((prev) => setDate(incrementMonth(prev, 1), 1))}
               >
                 <ChevronRightIcon className="w-5 h-5 " />
@@ -81,33 +133,9 @@ const CalendarInput = ({ label, className, value, setValue }: IProps): ReactElem
 
               {sliceArray(getCalendarDates(value), 7).map((chunk) => (
                 <div className="flex flex-row flex-nowrap gap-1" key={uuidv4()}>
-                  {chunk.map((day) =>
-                    areDatesEquals(value, day) ? (
-                      <div
-                        className={clsx([
-                          'flex justify-center items-center flex-grow w-10 py-1 border border-transparent rounded-sm bg-blue-600 text-white',
-                        ])}
-                        key={uuidv4()}
-                      >
-                        <p>{day.getDate()}</p>
-                      </div>
-                    ) : (
-                      <div
-                        className={clsx([
-                          'flex justify-center items-center w-10 py-1 flex-grow rounded-sm border border-transparent cursor-pointer',
-                          isDateInMonth(value, day)
-                            ? isSameDate(new Date(), day)
-                              ? 'bg-gray-400 text-gray-800 hover:border-gray-600'
-                              : 'bg-gray-200 text-gray-700 hover:border-gray-500'
-                            : 'bg-gray-100 text-gray-400 hover:border-gray-400',
-                        ])}
-                        onClick={handleClick.bind(this, day)}
-                        key={uuidv4()}
-                      >
-                        {day.getDate()}
-                      </div>
-                    )
-                  )}
+                  {chunk.map((day) => (
+                    <Cell currentDate={value} cellDate={day} onlyFuture={onlyFuture} handleClick={handleClick} key={uuidv4()} />
+                  ))}
                 </div>
               ))}
             </div>
