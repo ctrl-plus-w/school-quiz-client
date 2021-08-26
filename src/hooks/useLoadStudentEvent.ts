074abd5d825a6418380374c9c4d13beea8178ea7
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-26 16:48:32 +0200

    Added load student event and question hooks

---
+++ src/hooks/useLoadStudentEvent.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+
+import Router from 'next/router';
+
+import useAppSelector from '@hooks/useAppSelector';
+import useAppDispatch from '@hooks/useAppDispatch';
+
+import { getStudentEvent } from '@api/events';
+
+import { clearTempEvent, selectTempEvent, setTempEvent } from '@redux/eventSlice';
+import { selectToken } from '@redux/authSlice';
+import { selectUser } from '@redux/userSlice';
+
+interface IReturnProperties {
+  state: 'LOADING' | 'FULFILLED';
+  run: () => void;
+}
+
+const useLoadStudentEvent = (config?: { notFoundRedirect?: string; doNotRefetch?: boolean }, cbs?: Array<() => void>): IReturnProperties => {
+  const dispatch = useAppDispatch();
+
+  const [runner, setRunner] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const token = useAppSelector(selectToken);
+  const user = useAppSelector(selectUser);
+  const event = useAppSelector(selectTempEvent);
+
+  const run = () => setRunner(true);
+
+  useEffect(() => {
+    const fail = () => Router.push('/login');
+
+    (async () => {
+      if (!runner) return;
+
+      const compute = async () => {
+        if (!token || !user) return fail();
+
+        dispatch(clearTempEvent());
+
+        const [event, error] = await getStudentEvent(token);
+
+        if (error && error.status === 404 && config && config.notFoundRedirect) return Router.push(config.notFoundRedirect);
+        else if (error && error.status !== 404) return fail();
+
+        if (event) dispatch(setTempEvent(event));
+      };
+
+      if (!config || (config.doNotRefetch === true && !event) || !config?.doNotRefetch) compute();
+
+      if (cbs) for (const cb of cbs) cb();
+
+      setLoading(false);
+      setRunner(false);
+    })();
+  }, [runner]);
+
+  return { state: loading ? 'LOADING' : 'FULFILLED', run };
+};
+
+export default useLoadStudentEvent;
---
+++ src/hooks/useLoadStudentQuestion.ts
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react';
+
+import Router from 'next/router';
+
+import useAppSelector from '@hooks/useAppSelector';
+import useAppDispatch from '@hooks/useAppDispatch';
+
+import { getStudentQuestion } from '@api/questions';
+
+import { clearTempQuestion, selectTempQuestion, setTempQuestion } from '@redux/questionSlice';
+import { selectToken } from '@redux/authSlice';
+
+interface IReturnProperties {
+  state: 'LOADING' | 'FULFILLED';
+  run: () => void;
+}
+
+const useLoadStudentQuestion = (config?: { notFoundRedirect?: string; doNotRefetch?: boolean }, cbs?: Array<() => void>): IReturnProperties => {
+  const [runner, setRunner] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+
+  const dispatch = useAppDispatch();
+
+  const token = useAppSelector(selectToken);
+  const question = useAppSelector(selectTempQuestion);
+
+  const run = () => setRunner(true);
+
+  useEffect(() => {
+    const fail = () => Router.push('/login');
+
+    (async () => {
+      if (!runner) return;
+
+      const compute = async () => {
+        if (!token) return fail();
+
+        dispatch(clearTempQuestion());
+
+        const [question, error] = await getStudentQuestion(token);
+
+        if (error && error.status === 404 && config && config.notFoundRedirect) return Router.push(config.notFoundRedirect);
+        else if (error && error.status !== 404) return fail();
+
+        if (question) dispatch(setTempQuestion(question));
+      };
+
+      console.log(question);
+
+      if (!config || (config.doNotRefetch === true && !question) || !config?.doNotRefetch) compute();
+
+      if (cbs) for (const cb of cbs) cb();
+
+      setLoading(false);
+      setRunner(false);
+    })();
+  }, [runner]);
+
+  return { state: loading ? 'LOADING' : 'FULFILLED', run };
+};
+
+export default useLoadStudentQuestion;
