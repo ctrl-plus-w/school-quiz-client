8f516162ff3a7987bf46588580427eb3e732be9d
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-08-28 23:08:47 +0200

    Added an array utils function and made some docs

--- src/utils/array.utils.ts
+++ src/utils/array.utils.ts
@@ -1,7 +1,18 @@
+/**
+ * Get a random element in the array
+ * @param arr The array to get an element from
+ * @returns A array elementa
+ */
 export const random = <T>(arr: Array<T>): T => {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
+/**
+ * Slice the array into chunks
+ * @param arr The array to slice
+ * @param chunkSize The size of the chunks
+ * @returns An array of arrays
+ */
 export const sliceArray = <T>(arr: Array<T>, chunkSize: number): Array<Array<T>> => {
   const result = [];
 
@@ -10,4 +21,13 @@ export const sliceArray = <T>(arr: Array<T>, chunkSize: number): Array<Array<T>>
   }
 
   return result;
+};
+
+/**
+ * Check if the array has duplicated elements
+ * @param arr The arrary
+ * @returns A boolean
+ */
+export const hasDuplicatedElements = (arr: Array<unknown>): boolean => {
+  return arr.length !== new Set(arr).size;
 };
