e1b7c667f7a12d2dd1d5e28255bbc536a293f0e4
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-09-05 14:18:18 +0200

    Used the classes as string insteaf of vars to be detected in the purge

--- src/constants/notification.tsx
+++ src/constants/notification.tsx
@@ -2,30 +2,28 @@ import React from 'react';
 
 import { CheckCircleIcon, XCircleIcon, ExclamationCircleIcon } from '@heroicons/react/outline';
 
-const ICON_CLASSNAME = 'w-5 h-5 mx-3';
-
 export default {
   ERROR: {
     CLASSNAME: 'border-red-600 text-red-600',
-    ICON: <XCircleIcon className={ICON_CLASSNAME} />,
+    ICON: <XCircleIcon className="w-5 h-5 mx-3" />,
     TITLE: 'Erreur',
   },
 
   WARNING: {
     CLASSNAME: 'border-yellow-500 text-yellow-500',
-    ICON: <XCircleIcon className={ICON_CLASSNAME} />,
+    ICON: <XCircleIcon className="w-5 h-5 mx-3" />,
     TITLE: 'Attention',
   },
 
   INFO: {
     CLASSNAME: 'border-blue-600 text-blue-600',
-    ICON: <CheckCircleIcon className={ICON_CLASSNAME} />,
+    ICON: <CheckCircleIcon className="w-5 h-5 mx-3" />,
     TITLE: 'Information',
   },
 
   SUCCESS: {
     CLASSNAME: 'border-green-600 text-green-600',
-    ICON: <ExclamationCircleIcon className={ICON_CLASSNAME} />,
+    ICON: <ExclamationCircleIcon className="w-5 h-5 mx-3" />,
     TITLE: 'Information',
   },
 };
