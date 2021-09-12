eef3a5f53126418183d2ee38e8f47484654589a7
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-09-12 15:37:51 +0200

    Added a getUsers and a getRole api call

--- src/api/users.ts
+++ src/api/users.ts
@@ -4,3 +4,8 @@ export const getUsers = async (token: string, role?: 'professeur' | 'admin' | 'e
   const endpoint = role ? `/api/users?role=${role}&self=false` : '/api/users';
   return getApiCall(endpoint, token);
 };
+
+export const getUser = async (userId: number, token: string): Promise<APIResponse<IUser>> => {
+  const endpoint = `/api/users/${userId}`;
+  return getApiCall(endpoint, token, { errors: { '404': "L'utilisateur n'a pas été trouvé." } });
+};
