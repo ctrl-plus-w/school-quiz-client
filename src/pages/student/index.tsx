2bbad22213dcc11ccc4e53fb7fc70da40f617116
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-09-13 20:34:16 +0200

    Added the events on the student home page

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -4,27 +4,37 @@ import type { ReactElement } from 'react';
 
 import React from 'react';
 
-import Title from '@element/Title';
-
 import StudentDashboardSkeleton from '@layout/StudentDashboardSkeleton';
 import StudentDashboard from '@layout/StudentDashboard';
+
+import Table from '@module/Table';
+
+import Subtitle from '@element/Subtitle';
+import Title from '@element/Title';
 
 import TitleSkeleton from '@skeleton/TitleSkeleton';
 import TextSkeleton from '@skeleton/TextSkeleton';
 
 import useAuthentication from '@hooks/useAuthentication';
+import useLoadEvents from '@hooks/useLoadEvents';
 import useLoading from '@hooks/useLoading';
 
+import { maxScoreMapper, quizTitleMapper, scoreMapper } from '@util/mapper.utils';
+import { formatDateTime } from '@util/date.utils';
+
+import { selectEvents } from '@redux/eventSlice';
 import { selectUser } from '@redux/userSlice';
 
 import ROLES from '@constant/roles';
 
 const Student = (): ReactElement => {
-  const { state } = useAuthentication(ROLES.STUDENT.PERMISSION);
+  const { state: eventsState, run: runEvents } = useLoadEvents({ onNotFoundDoNothing: true });
+  const { state: authState } = useAuthentication(ROLES.STUDENT.PERMISSION, [runEvents]);
 
-  const { loading } = useLoading([state]);
+  const { loading } = useLoading([authState, eventsState]);
 
   const user = useSelector(selectUser);
+  const events = useSelector(selectEvents);
 
   return loading || !user ? (
     <StudentDashboardSkeleton>
@@ -35,12 +45,21 @@ const Student = (): ReactElement => {
     </StudentDashboardSkeleton>
   ) : (
     <StudentDashboard>
-      <div className="flex flex-col py-12 px-12">
+      <div className="flex flex-col w-full h-full py-12 px-12">
         <Title>Bienvenue, {user.firstName} !</Title>
+        <Subtitle>Vous avez sur cette page les informations globales ainsi que les informations les plus importantes.</Subtitle>
 
-        <p className="text-gray-600 font-normal mt-4">
-          Vous avez sur cette page les informations globales ainsi que les informations les plus importantes.
-        </p>
+        <Table<IEvent, keyof IEvent>
+          attributes={[
+            ['ID', 'id'],
+            ['Titre', 'quiz', quizTitleMapper],
+            ['Début', 'start', (start) => formatDateTime(new Date(start))],
+            ['Score', 'analytics', scoreMapper],
+            ['Score max', 'analytics', maxScoreMapper],
+          ]}
+          data={events}
+          handleClick={() => null}
+        />
       </div>
     </StudentDashboard>
   );
