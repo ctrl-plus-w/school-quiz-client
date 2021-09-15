7a253b1ee7b03942a5741b95616a1abd61524516
ctrl-plus-w <lukas.ldrn@gmail.com>
2021-09-15 14:16:55 +0200

    Made the update user computations

--- src/pages/admin/users/[id].tsx
+++ src/pages/admin/users/[id].tsx
@@ -28,7 +28,7 @@ import useLoadRoles from '@hooks/useLoadRoles';
 import useLoadUser from '@hooks/useLoadUser';
 import useLoading from '@hooks/useLoading';
 
-import { genderMapper, nameSlugMapper } from '@util/mapper.utils';
+import { genderMapper, nameSlugMapper, slugMapper } from '@util/mapper.utils';
 
 import { selectGroups } from '@redux/groupSlice';
 import { selectRoles } from '@redux/roleSlice';
@@ -41,9 +41,17 @@ import FormGroupSkeleton from '@skeleton/FormGroupSkeleton';
 import TitleSkeleton from '@skeleton/TitleSkeleton';
 import InputSkeleton from '@skeleton/InputSkeleton';
 import FormSkeleton from '@skeleton/FormSkeleton';
+import { areArraysEquals } from '@util/condition.utils';
+import { getLength } from '@util/object.utils';
+import useAppDispatch from '@hooks/useAppDispatch';
+import { addUserGroups, removeUserGroup, updateUser, updateUserRole } from '@api/users';
+import { addErrorNotification, addSuccessNotification } from '@redux/notificationSlice';
+import PasswordInput from '@element/PasswordInput';
 
 const User = (): ReactElement => {
   const router = useRouter();
+
+  const dispatch = useAppDispatch();
 
   const { id } = router.query;
 
@@ -64,15 +72,29 @@ const User = (): ReactElement => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [gender, setGender] = useState('');
 
   const [role, setRole] = useState('');
   const [userGroups, setUserGroups] = useState<Array<IBasicModel>>([]);
 
   const { valid } = useValidation(
-    () => false,
-    [firstName, lastName, username, gender, role, userGroups],
-    [firstName, lastName, username, gender, role]
+    () => {
+      if (!user) return false;
+
+      const genderAssociations: { [index: string]: boolean } = { homme: true, femme: false };
+      const genderToBoolean = typeof genderAssociations[gender] === 'boolean' ? genderAssociations[gender] : null;
+
+      if (firstName !== user.firstName || lastName !== user.lastName || password !== '' || genderToBoolean !== user.gender) return true;
+
+      if (user.role && user.role.slug !== role) return true;
+
+      if (!areArraysEquals(user.groups.map(slugMapper), userGroups.map(slugMapper))) return true;
+
+      return false;
+    },
+    [firstName, lastName, username, gender, role, userGroups, password],
+    [firstName, lastName, username, role]
   );
 
   useEffect(() => {
@@ -93,14 +115,61 @@ const User = (): ReactElement => {
     setFormFilled(true);
   }, [user]);
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    if (!valid || !token) return;
+    if (!valid || !token || !user) return;
 
-    //  TODO : Make the update user computations.
+    const handleError = (nullAssertionValue: unknown, error?: { status: number; message: string }): boolean => {
+      if (!nullAssertionValue || error) {
+        if (error && error.status === 403) router.push('/login');
+        dispatch(addErrorNotification(error?.message || 'Une erreur est survenue.'));
 
-    alert(1);
+        return true;
+      }
+
+      return false;
+    };
+
+    const userUpdateAttributes: AllOptional<IUserCreationAttributes> = {
+      username: username !== '' && username !== user.username ? username : undefined,
+      firstName: firstName !== '' && firstName !== user.firstName ? firstName : undefined,
+      lastName: lastName !== '' && lastName !== user.lastName ? lastName : undefined,
+      password: password !== '' ? password : undefined,
+    };
+
+    if (getLength(userUpdateAttributes) > 0) {
+      const [updatedUser, updateUserError] = await updateUser(user.id, userUpdateAttributes, token);
+      if (handleError(updatedUser, updateUserError)) return;
+    }
+
+    const newRole = roles.find(({ slug }) => role === slug);
+
+    if ((!user.role || role !== user.role.slug) && newRole) {
+      const [updatedRole, updateRoleError] = await updateUserRole(user.id, newRole.id, token);
+      if (handleError(updatedRole, updateRoleError)) return;
+    }
+
+    const newGroups = groups.filter(
+      ({ slug }) => !user.groups.some((_group) => _group.slug === slug) && userGroups.some((_group) => _group.slug === slug)
+    );
+
+    if (newGroups.length > 0) {
+      const [groupsUpdated, addGroupError] = await addUserGroups(user.id, newGroups.map((group) => group.id) as number[], token);
+      if (handleError(groupsUpdated, addGroupError)) return;
+    }
+
+    const oldGroups = user.groups.filter((group) => !userGroups.some((_group) => _group.slug === group.slug));
+
+    if (oldGroups.length > 0) {
+      for (const group of oldGroups) {
+        const [groupsUpdated, removeGroupError] = await removeUserGroup(user.id, group.id as number, token);
+        if (handleError(groupsUpdated, removeGroupError)) return;
+      }
+    }
+
+    dispatch(addSuccessNotification('Utilisateur modifié.'));
+    router.push('/admin/users');
   };
 
   return loading || !formFilled || !user ? (
@@ -145,6 +214,7 @@ const User = (): ReactElement => {
               <Input label="Prénom" setValue={setFirstName} value={firstName} placeholder="John" autofocus />
               <Input label="Nom" setValue={setLastName} value={lastName} placeholder="Doe" />
               <Input label="Nom d'utilisateur" setValue={setUsername} value={username} placeholder="jdoe" />
+              <PasswordInput label="Mot de passe" setValue={setPassword} value={password} placeholder="*****" generator />
 
               <Dropdown
                 label="Sexe"
